96247c0015d5bdfc63dcbffd42cd64e4c97f10be
Raymonidle <variluoxx@gmail.com>
2024-10-04 11:21:20 -0700

    did problem 1` and some of problem 2

--- Main.java
+++ Main.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
 public class Main
 {
   public static void main(String[] args)
   {
+    //System.out.println(lastFirstN());
+    stringManip();
+
     // write your code here
+  }
+
+  public static String lastFirstN(){
+    Scanner userInput = new Scanner(System.in);
+
+    System.out.println("Whats your first word?");
+    String s1 = userInput.nextLine();
+
+    System.out.println("Whats your second word?");
+    String s2 = userInput.nextLine();
+
+    System.out.println("What number do you want?");
+    int n = userInput.nextInt();
+
+    s1 = s1.substring(n-1);
+    s2 = s2.substring(0, s2.length() - n);
+    return s1 + s2;
+  }
+
+  public static void stringManip(String s1, String s2){
+    s1.toUpperCase();
+    System.out.println(s1);
+
+    s2.substring(0)
   }
 }
